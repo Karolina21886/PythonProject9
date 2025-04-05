adddf54e333dbb5e7710662872bf58f110edce3d
User <stroikarolina12@gmail.com>
2025-04-05 12:37:33 +0300

    1 дз

--- ДЗ.py
+++ ДЗ.py
@@ -1,4 +1,4 @@
-a
+
     if 0 <= score <= 49:
         return "Незадовільно"
     if 50 <= score <= 69:
@@ -7,11 +7,3 @@ a
         return "Добре"
     if 0 <= 90:
         return "Відмінно"
-    :
-        return "Некоректне значення балів"
-
-try:
-    score = int(input("Введіть кількість балів (0-100): "))
-    print(f"Ваша оцінка: {get_grade(score)}")
-except ValueError:
-    print("Будь ласка, введіть ціле число від 0 до 100.")
